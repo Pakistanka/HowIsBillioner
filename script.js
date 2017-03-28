0ee6cb90778238047cb52d50840767d3fc508921
¨Anastasia <polozova.anastasi@gmail.com>
2017-03-28 09:48:05 +0100

    Доделала игру "Кто хочет стать миллионером?"

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function numberToObject(number) {
 console.log(numberToObject(number));
 
 /*--------------------
---------TASK 2--------
+--------TASK 3--------
 --------------------*/
 
 var questionText = [];
@@ -37,45 +37,45 @@ var moneyWon = [];
 var toBreak = false;
 var moneyCheckpoint = "0$"
 
-questionText[0] = '';
-answerFirst [0] = '';
-answerSecond [0] = '';
-answerThird [0] = '';
-answerFourth [0] = '';
-rightAnswer [0] = '';
-moneyWon [0] = '';
+questionText[0] = 'Сколько лап у змеи?';
+answerFirst [0] = 'а) 1';
+answerSecond [0] = 'б) 2';
+answerThird [0] = 'в) 0';
+answerFourth [0] = 'г) 3';
+rightAnswer [0] = 'в';
+moneyWon [0] = '100$';
 
-questionText[1] = '';
-answerFirst [1] = '';
-answerSecond [1] = '';
-answerThird [1] = '';
-answerFourth [1] = '';
-rightAnswer [1] = '';
-moneyWon [1] = '';
+questionText[1] = 'Как называется столица Республики Камерун?';
+answerFirst [1] = 'а) Котону';
+answerSecond [1] = 'б) Яунду';
+answerThird [1] = 'в) Дуала';
+answerFourth [1] = 'г) Касабланка';
+rightAnswer [1] = 'б';
+moneyWon [1] = '200$';
 
-questionText[2] = '';
-answerFirst [2] = '';
-answerSecond [2] = '';
-answerThird [2] = '';
-answerFourth [2] = '';
-rightAnswer [2] = '';
-moneyWon [2] = '';
+questionText[2] = 'Где находится Мексика?';
+answerFirst [2] = 'а) Азия';
+answerSecond [2] = 'б) Африка';
+answerThird [2] = 'в) Южная Америка';
+answerFourth [2] = 'г) Северная Америка';
+rightAnswer [2] = 'г';
+moneyWon [2] = '300$';
 
-questionText[3] = '';
-answerFirst [3]3 = '';
-answerSecond [3] = '';
-answerThird [3] = '';
-answerFourth [3] = '';
-rightAnswer [3] = '';
-moneyWon [3] = '';
+questionText[3] = 'Где находится Самарканд?';
+answerFirst [3] = 'а) в Узбекистане';
+answerSecond [3] = 'б) в Киргизии';
+answerThird [3] = 'в) в Афганистане';
+answerFourth [3] = 'г) в Иране';
+rightAnswer [3] = 'а';
+moneyWon [3] = '400$';
 
-questionText[4] = '';
-answerFirst [4] = '';
-answerSecond [4] = '';
-answerThird [4] = '';
-answerFourth [4] = '';
-rightAnswer [4] = '';
-moneyWon [4] = '';
+questionText[4] = 'Какое название носил Стамбул до 1492 года?';
+answerFirst [4] = 'а) Бурса';
+answerSecond [4] = 'б) Константинополь';
+answerThird [4] = 'в) Восточная Империя';
+answerFourth [4] = 'г) Третий Рим';
+rightAnswer [4] = 'б';
+moneyWon [4] = '500$';
 
 function userQuestion(i) {
         userAnswer = prompt("Вопрос: " + questionText[i] + "\n" + "Ответы: " + "\n" + answerFirst[i] + "\n" + answerSecond[i] + "\n" + answerThird[i] + "\n" + answerFourth[i]) 
